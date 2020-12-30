b8f5923bee70332f2c796221b5ad27a287ab0997
Peter Runge <prunge@causal.net.au>
2020-12-30 22:49:33 +1100

    Java task, configuration of file inputs and outputs.

---
+++ src/main/java/au/com/helixta/adl/gradle/AdlGenerateJavaTask.java
@@ -0,0 +1,42 @@
+package au.com.helixta.adl.gradle;
+
+import org.gradle.api.Action;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.tasks.Nested;
+import org.gradle.api.tasks.TaskAction;
+
+import javax.inject.Inject;
+
+public class AdlGenerateJavaTask extends DefaultTask
+{
+    private final AdlPluginExtension.Generations generations;
+
+    @Inject
+    public AdlGenerateJavaTask(ObjectFactory objectFactory)
+    {
+        //this.generations =  getExtensions().create("generations", AdlPluginExtension.Generations.class, objectFactory);
+        this.generations = objectFactory.newInstance(AdlPluginExtension.Generations.class);
+    }
+
+    @Nested
+    public AdlPluginExtension.Generations getGenerations()
+    {
+        return generations;
+    }
+
+    public void generations(Action<? super AdlPluginExtension.Generations> configuration)
+    {
+        configuration.execute(generations);
+    }
+
+    @TaskAction
+    public void generateJava()
+    {
+        for (AdlPluginExtension.JavaGeneration java : getGenerations().getJava())
+        {
+            System.out.println("Generate java: " + java.getOutputDirectory().get());
+            System.out.println("   Files: " + java.getSourcepath().getFiles());
+        }
+    }
+}
--- src/main/java/au/com/helixta/adl/gradle/AdlPluginExtension.java
+++ src/main/java/au/com/helixta/adl/gradle/AdlPluginExtension.java
@@ -1,24 +1,32 @@
 package au.com.helixta.adl.gradle;
 
 import org.gradle.api.Action;
+import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileTree;
+import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.plugins.ExtensionAware;
-import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Nested;
 import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.PathSensitive;
+import org.gradle.api.tasks.PathSensitivity;
+import org.gradle.api.tasks.SkipWhenEmpty;
+import org.gradle.api.tasks.util.PatternSet;
+import org.gradle.internal.Factory;
 
 import javax.inject.Inject;
 import java.io.File;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AdlPluginExtension implements ExtensionAware
 {
-    @Nested
     private final Generations generations = getExtensions().create("generations", Generations.class);
 
+    @Nested
     public Generations getGenerations()
     {
         return generations;
@@ -31,12 +39,13 @@ public abstract class AdlPluginExtension implements ExtensionAware
 
     public abstract static class Generations implements ExtensionAware
     {
-        @Nested
         private final List<JavaGeneration> java = new ArrayList<>();
 
         @Inject
         protected abstract ObjectFactory getObjectFactory();
 
+        @Nested
+        @SkipWhenEmpty
         public List<JavaGeneration> getJava()
         {
             return java;
@@ -52,10 +61,66 @@ public abstract class AdlPluginExtension implements ExtensionAware
 
     public static abstract class Generation
     {
-        private final DirectoryProperty outputDirectory = getObjectFactory().directoryProperty();
+        private final DirectoryProperty outputDirectory =
+                getObjectFactory().directoryProperty()
+                    .convention(getProjectLayout().getBuildDirectory().dir("generated/adl/" + generationType()));
+
+        private final ConfigurableFileCollection sourcepath =
+                getObjectFactory().fileCollection();
+
+        private final PatternSet patternSet = getPatternSetFactory().create().include("**/*.adl");
 
         @Inject
         protected abstract ObjectFactory getObjectFactory();
+
+        @Inject
+        protected abstract ProjectLayout getProjectLayout();
+
+        @Inject
+        protected abstract Factory<PatternSet> getPatternSetFactory();
+
+        protected abstract String generationType();
+
+        @PathSensitive(PathSensitivity.RELATIVE)
+        @InputFiles
+        public FileTree getSourcepath()
+        {
+            return sourcepath.getAsFileTree().matching(patternSet);
+        }
+
+        public void sourcepath(File... sources)
+        {
+            sourcepath.from((Object[]) sources);
+        }
+
+        public void sourcepath(FileCollection... sources)
+        {
+            sourcepath.from((Object[]) sources);
+        }
+
+        public Generation include(String... includes)
+        {
+            patternSet.include(includes);
+            return this;
+        }
+
+        public Generation include(Iterable<String> includes)
+        {
+            patternSet.include(includes);
+            return this;
+        }
+
+        public Generation exclude(String... excludes)
+        {
+            patternSet.exclude(excludes);
+            return this;
+        }
+
+        public Generation exclude(Iterable<String> excludes)
+        {
+            patternSet.exclude(excludes);
+            return this;
+        }
 
         @OutputDirectory
         public DirectoryProperty getOutputDirectory()
@@ -66,6 +131,10 @@ public abstract class AdlPluginExtension implements ExtensionAware
 
     public abstract static class JavaGeneration extends Generation
     {
-
+        @Override
+        protected String generationType()
+        {
+            return "java";
+        }
     }
 }
