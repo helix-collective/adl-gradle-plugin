7d46f554c57097dc929f71ae7005fcb67664c5f5
Peter Runge <prunge@causal.net.au>
2021-02-15 09:15:55 +1100

    Integration test for the convention type of setup/configuration of the ADL plugin.

---
+++ src/gradleTest/adljavaconvention/build.gradle.kts
@@ -0,0 +1,62 @@
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    `java-library`
+    id("au.com.helixta.adl")
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+adl {
+    generations {
+        java {
+            javaPackage  = "adl.test"
+            isGenerateTransitive = true
+            isGenerateAdlRuntime = true
+            headerComment = """
+                Full
+                of
+                galahs
+            """.trimIndent()
+        }
+    }
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.8.6")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.assertj:assertj-core:3.18.1")
+    testImplementation("com.github.javaparser:javaparser-core:3.18.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks {
+    clean {
+        delete("$projectDir/generated")
+    }
+
+    processTestResources {
+        from(sourceSets.main.get().allSource)
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    register("runGradleTest") {
+        dependsOn(check)
+    }
+}
