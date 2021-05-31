a00d34f6c50497ea007745ce83b2ea97020254f4
Peter Runge <prunge@causal.net.au>
2021-05-31 23:19:07 +1000

    Use 'by' syntax for registering source sets and tasks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,36 +46,7 @@ open class GradleFunctionalTest : Test() {
     }
 }
 
-val functionalTest = sourceSets.create("functionalTest")
-val functionalTestTask = tasks.register<GradleFunctionalTest>("functionalTest") {
-    group = "verification"
-    testClassesDirs = functionalTest.output.classesDirs
-    classpath = functionalTest.runtimeClasspath
-    useJUnitPlatform()
-    systemProperty("test.projectworkspace.directory", layout.buildDirectory.dir("functest").get().asFile.path)
-    systemProperty("test.testkit.directory", layout.projectDirectory.dir("build-home").asFile.path)
-    outputs.dir(layout.buildDirectory.dir("functest"))
-    workingDir(layout.buildDirectory.dir("functest"))
-
-    addTestListener(object : TestListener {
-        override fun beforeSuite(suite: TestDescriptor) {}
-        override fun beforeTest(testDescriptor: TestDescriptor) {}
-        //Print names of tests after they run
-        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
-            if (result.resultType != TestResult.ResultType.SKIPPED || gradleTestFilteringUsed()) {
-                logger.lifecycle("\t${result.resultType} - ${testDescriptor.className?.substringAfterLast(".")} : ${testDescriptor.displayName}")
-            }
-        }
-        //Print suite class names that are full of skipped tests that indicate they contain tests that cannot run on this platform
-        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
-            if (!gradleTestFilteringUsed()) {
-                if (suite.className != null && result.skippedTestCount > 0) {
-                    logger.warn("\tIntegration tests in ${suite.name} cannot run on this platform - they are being skipped.")
-                }
-            }
-        }
-    })
-}
+val functionalTest by sourceSets.creating
 
 dependencies {
     implementation("com.github.docker-java:docker-java-core:3.2.7")
@@ -90,20 +61,50 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-tasks.register<Delete>("cleanFull") {
-    dependsOn(tasks.clean)
-    description = "Deletes the build directory and the functional test workspace home directory"
-    group = "build"
-    delete("build-home")
-}
-
 tasks {
+    val cleanFull by registering(Delete::class) {
+        dependsOn(clean)
+        description = "Deletes the build directory and the functional test workspace home directory"
+        group = "build"
+        delete("build-home")
+    }
+
+    val runFunctionalTests by registering(GradleFunctionalTest::class) {
+        group = "verification"
+        testClassesDirs = functionalTest.output.classesDirs
+        classpath = functionalTest.runtimeClasspath
+        useJUnitPlatform()
+        systemProperty("test.projectworkspace.directory", layout.buildDirectory.dir("functest").get().asFile.path)
+        systemProperty("test.testkit.directory", layout.projectDirectory.dir("build-home").asFile.path)
+        outputs.dir(layout.buildDirectory.dir("functest"))
+        workingDir(layout.buildDirectory.dir("functest"))
+
+        addTestListener(object : TestListener {
+            override fun beforeSuite(suite: TestDescriptor) {}
+            override fun beforeTest(testDescriptor: TestDescriptor) {}
+            //Print names of tests after they run
+            override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
+                if (result.resultType != TestResult.ResultType.SKIPPED || gradleTestFilteringUsed()) {
+                    logger.lifecycle("\t${result.resultType} - ${testDescriptor.className?.substringAfterLast(".")} : ${testDescriptor.displayName}")
+                }
+            }
+            //Print suite class names that are full of skipped tests that indicate they contain tests that cannot run on this platform
+            override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+                if (!gradleTestFilteringUsed()) {
+                    if (suite.className != null && result.skippedTestCount > 0) {
+                        logger.warn("\tIntegration tests in ${suite.name} cannot run on this platform - they are being skipped.")
+                    }
+                }
+            }
+        })
+    }
+
     test {
         useJUnitPlatform()
     }
 
     check {
-        dependsOn(functionalTestTask)
+        dependsOn(runFunctionalTests)
     }
 
     gradlePlugin {
